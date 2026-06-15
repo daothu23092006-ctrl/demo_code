227ed16a4aa8b76494573a6b357a9a7819358593
RintaroTsumugi <nguyenthanhtuyen02072006@gmail.com>
2026-06-16 01:21:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -247,14 +247,14 @@ else:
 """, unsafe_allow_html=True)
 
             st.write("")
-"""
-       with st.expander("🔧 Debug — meal_targets & filters"):
-            st.json({
-                "tdee_final": round(tdee_adj, 1),
-                "meal_targets": targets,
-                "filters": choices,
-            })
-"""
+
+       # with st.expander("🔧 Debug — meal_targets & filters"):
+       #     st.json({
+       #         "tdee_final": round(tdee_adj, 1),
+       #         "meal_targets": targets,
+       #         "filters": choices,
+           #    })
+
 
         st.write("") 
         # Khi nhấn nút này, hệ thống sẽ rerun và gọi lại hàm gợi ý ngẫu nhiên món khác dựa trên bộ lọc đã chọn trong choices
